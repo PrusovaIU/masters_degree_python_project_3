f5516496fd6505928a6c325e2984dc47892e76b4
Iuliia <prusova.work@gmail.com>
2025-11-16 01:04:59 +0300

    добавлен argparser

--- main.py
+++ main.py
@@ -1,27 +1,62 @@
 from valutatrade_hub.config import Config
 from valutatrade_hub.cli.interface import Engine
-from valutatrade_hub.core.log_record import BalanceLogRecord
 from valutatrade_hub.logger import Logger
 from valutatrade_hub.parser_service.config import ParserConfig
 from valutatrade_hub.parser_service.api_clients import init_clients
 from valutatrade_hub.parser_service.updater import RatesUpdater
+from argparse import ArgumentParser
 
 
-if __name__ == '__main__':
-    config = Config(
-        "/home/hex/git/masters_degree_python_project_3/config.json")
+def main(
+        config_path: str,
+        ps_config_path: str,
+        logger_config_path: str
+):
+    config = Config(config_path)
     config.load()
-    parser_config = ParserConfig("/home/hex/git/masters_degree_python_project_3/parser_config.json")
+    parser_config = ParserConfig(ps_config_path)
     parser_config.load()
-    clients = init_clients(parser_config)
-    logger_config = Logger(
-        "/home/hex/git/masters_degree_python_project_3/"
-        "logger_config.json"
-    )
-    logger_config.load()
-    updater = RatesUpdater(parser_config, logger_config, *clients)
-    updater.run_update()
+    logger = Logger(logger_config_path)
+    logger.load()
+    updater = _init_parser_service(parser_config, logger)
     try:
         Engine(config, updater).run()
     except KeyboardInterrupt:
         print("Завершение работы...")
+
+
+def _init_parser_service(
+        parser_config: ParserConfig,
+        logger: Logger
+) -> RatesUpdater:
+    clients = init_clients(parser_config)
+    updater = RatesUpdater(parser_config, logger, *clients)
+    updater.run_update()
+    return updater
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description="ValutaTrade Hub")
+    parser.add_argument(
+        "--config",
+        type=str,
+        dest="config",
+        required=True,
+        help="Путь к файлу конфигурации"
+    )
+    parser.add_argument(
+        "--ps-config",
+        type=str,
+        dest="ps_config",
+        required=True,
+        help="Путь к файлу конфигурации парсера"
+    )
+    parser.add_argument(
+        "--logger-config",
+        type=str,
+        dest="logger_config",
+        required=True,
+        help="Путь к файлу конфигурации логгера"
+    )
+    args = parser.parse_args()
+    main(args.config, args.ps_config, args.logger_config)
